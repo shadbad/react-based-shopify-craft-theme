047ae3ed1f0b46a65f4e82cd5523603f6e5ab1cf
Sina Shadbad <shadbad@gmail.com>
2022-08-07 12:40:49 +0100

    add routes to the index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,19 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { Layout } from 'components/templates';
-
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Landing } from 'components/pages';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
     <React.StrictMode>
-        <Layout>
-            <p>This is a sample child inside the body</p>
-        </Layout>
+        <Router>
+            <Routes>
+                <Route path="/" element={<Landing />} />
+            </Routes>
+        </Router>
     </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
