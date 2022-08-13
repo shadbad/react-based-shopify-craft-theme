a9570a29dda6ecccdd5a62afb2fd04787192456a
Sina Shadbad <shadbad@gmail.com>
2022-08-13 09:33:25 +0100

    refactor the iconLink atom.

--- src/components/atoms/iconLink/iconLink.jsx
+++ src/components/atoms/iconLink/iconLink.jsx
@@ -1,27 +1,25 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+
 import styles from './icon-link.module.scss';
 
 const IconLink = React.memo(function ({ className, url, iconName, clickHandler, badge }) {
 
     const showBadge = badge !== '';
 
-    const classNames = `${showBadge ? styles['link--show-badge'] : styles['link']} icon-${iconName} ${className}`;
+    const classNames = `${styles.link} ${showBadge ? styles.showBadge : ''} icon-${iconName} ${className}`;
 
     return (
+
         <Link to={url} className={classNames} onClick={clickHandler}>
-            {showBadge && <i className={styles['badge']}>{badge}</i>}
+
+            {showBadge && <i className={styles.badge}>{badge}</i>}
+
         </Link>
+
     );
 
-});
-
-IconLink.model = PropTypes.shape({
-    url: PropTypes.string.isRequired,
-    iconName: PropTypes.string.isRequired,
-    clickHandler: PropTypes.func,
-    badge: PropTypes.string
 });
 
 IconLink.propTypes = {
