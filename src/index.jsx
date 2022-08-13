6326a41de12175d570cb017059beaa5516cb67d1
Sina Shadbad <shadbad@gmail.com>
2022-08-13 07:10:12 +0100

    update the main index file's routes.

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { Landing } from 'components/pages';
+import { Landing, Collection } from 'pages';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -11,6 +11,7 @@ root.render(
         <Router>
             <Routes>
                 <Route path="/" element={<Landing />} />
+                <Route path="/collections/:slug" element={<Collection />} />
             </Routes>
         </Router>
     </React.StrictMode>
