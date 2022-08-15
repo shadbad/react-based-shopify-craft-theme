be1fbd74da87f53d7357c2ac9c8cdacf80a6d9b7
Sina Shadbad <shadbad@gmail.com>
2022-08-15 18:21:06 +0100

    Add the text box atom

--- src/components/atoms/index.js
+++ src/components/atoms/index.js
@@ -3,3 +3,4 @@ export { Heading } from './heading/heading';
 export { IconLink } from './iconLink/iconLink';
 export { NavigationLink } from './navigationLink/navigationLink';
 export { SocialLink } from './socialLink/socialLink';
+export { TextBox } from './textBox/textBox';
