8026c243615267cb8c2b2e75a60e170293410d9f
Sina Shadbad <shadbad@gmail.com>
2022-08-15 18:20:22 +0100

    Add the search result page

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { Landing, Collection } from 'pages';
+import { Landing, Collection, Search } from 'pages';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -12,6 +12,7 @@ root.render(
             <Routes>
                 <Route path="/" element={<Landing />} />
                 <Route path="/collections/:slug" element={<Collection />} />
+                <Route path="/search/:query" element={<Search />} />
             </Routes>
         </Router>
     </React.StrictMode>
