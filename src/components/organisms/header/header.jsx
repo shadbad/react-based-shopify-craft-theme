6d8009e02e3ed7ad0d17300bc9bbbc28ff849364
Sina Shadbad <shadbad@gmail.com>
2022-08-16 04:41:22 +0100

    Refactor the header organism

--- src/components/organisms/header/header.jsx
+++ src/components/organisms/header/header.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useReducer } from 'react';
 import { Announcement } from 'components/atoms';
 import { HeaderDrawer, NavBar, SearchBar } from 'components/molecules';
 import { useDataProvider } from 'hooks';
@@ -7,31 +7,28 @@ import styles from './header.module.scss';
 
 const Header = React.memo(function () {
 
-    const [menuIsExpanded, setMenuIsExpanded] = useState(false);
-
-    const [searchBarVisibility, setSearchBarVisibility] = useState(false);
-
     const announcement = useDataProvider('announcement');
 
     const categories = useDataProvider('categories');
 
     const socialLinks = useDataProvider('SOCIAL_PLATFORMS');
 
-    const eventHandlers = {
+    const reducer = function (state, action) {
 
-        menuClick() {
+        switch (action.type) {
 
-            setMenuIsExpanded((prevMenuIsExpanded) => !prevMenuIsExpanded);
-
-        },
-
-        searchClick() {
-
-            setSearchBarVisibility((prevSearchVisibility) => !prevSearchVisibility);
+            case 'toggleMenu':
+                return { ...state, menuIsExpanded: !state.menuIsExpanded };
+            case 'toggleSearch':
+                return { ...state, searchIsVisible: !state.searchIsVisible };
+            default:
+                throw new Error();
 
         }
 
     };
+
+    const [state, dispatch] = useReducer(reducer, { menuIsExpanded: false, searchIsVisible: false });
 
     return (
         <header>
@@ -39,21 +36,21 @@ const Header = React.memo(function () {
             {announcement.status === 'done' && <Announcement text={announcement.data.text} url={announcement.data.url} />}
 
             <HeaderDrawer
-                menuIsExpanded={menuIsExpanded}
-                menuClickHandler={eventHandlers.menuClick}
-                searchClickHandler={eventHandlers.searchClick}
+                menuIsExpanded={state.menuIsExpanded}
+                menuClickHandler={() => dispatch({ type: 'toggleMenu' })}
+                searchClickHandler={() => dispatch({ type: 'toggleSearch' })}
                 cartBadge="0"
             />
 
             <SearchBar
                 className={styles.header}
-                searchBarVisibility={searchBarVisibility}
-                setSearchBarVisibility={setSearchBarVisibility}
+                searchBarVisibility={state.searchIsVisible}
+                setSearchBarVisibility={() => dispatch({ type: 'toggleSearch' })}
             />
 
             {
                 (categories.status === 'done' && socialLinks.status === 'done') &&
-                <NavBar links={categories.data} menuIsExpanded={menuIsExpanded} socialLinks={socialLinks.data} />
+                <NavBar links={categories.data} menuIsExpanded={state.menuIsExpanded} socialLinks={socialLinks.data} />
             }
 
         </header>
