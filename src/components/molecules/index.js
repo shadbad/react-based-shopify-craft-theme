3645887b595a6d23b3320614753bf4fa08fd6650
Sina Shadbad <shadbad@gmail.com>
2022-08-25 08:40:16 +0100

    Add the linkIcon & linkArrow molecules.

--- src/components/molecules/index.js
+++ src/components/molecules/index.js
@@ -1,3 +1,6 @@
+export { LinkIcon } from './linkIcon/linkIcon';
+export { LinkArrow } from './linkArrow/LinkArrow';
+
 export { HeaderDrawer } from './headerDrawer/headerDrawer';
 export { NavBar } from './navBar/navBar';
 export { SearchBar } from './searchBar/searchBar';
