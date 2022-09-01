58a9c89fc0d7bd967ce2d9576c058c0d8c6f7737
Sina Shadbad <shadbad@gmail.com>
2022-09-01 12:52:18 +0100

    Add the heading type 4 atom.

--- src/components/atoms/textHeading/textHeading.jsx
+++ src/components/atoms/textHeading/textHeading.jsx
@@ -12,6 +12,9 @@ const TextHeading = React.memo(function ({ className, type, children }) {
         case 3:
             return <h3 className={`text-heading-${type} ${className}`}>{children}</h3>;
 
+        case 4:
+            return <h4 className={`text-heading-${type} ${className}`}>{children}</h4>;
+
         default:
             return <h1 className={`text-heading-${type} ${className}`}>{children}</h1>;
 
@@ -21,7 +24,7 @@ const TextHeading = React.memo(function ({ className, type, children }) {
 
 TextHeading.propTypes = {
     className: PropTypes.string,
-    type: PropTypes.oneOf([1, 2, 3]),
+    type: PropTypes.oneOf([1, 2, 3, 4]),
     children: PropTypes.node.isRequired
 };
 
