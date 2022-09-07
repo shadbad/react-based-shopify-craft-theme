983eb8338f7d6ab881debb0a83f193394d588e90
Sina Shadbad <shadbad@gmail.com>
2022-09-07 14:50:07 +0100

    Add the error page.

--- src/index.jsx
+++ src/index.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Layout } from 'components/organisms';
-import { Landing, Collection, Search } from 'pages';
+import { Landing, Collection, Search, ErrorBoundary } from 'pages';
 import { Provider } from 'react-redux';
 import store from 'store/store';
 import initializeStore from 'store/initialize';
@@ -14,17 +14,19 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
 
-    <Provider store={store}>
-        <Router basename="/react-based-shopify-craft-theme/">
-            <Layout>
-                <Routes>
-                    <Route path="/" element={<Landing />} />
-                    <Route path="/collections/:slug" element={<Collection />} />
-                    <Route path="/search/:query" element={<Search />} />
-                </Routes>
-            </Layout>
-        </Router>
-    </Provider>
+    <ErrorBoundary>
+        <Provider store={store}>
+            <Router basename="/react-based-shopify-craft-theme/">
+                <Layout>
+                    <Routes>
+                        <Route path="/" element={<Landing />} />
+                        <Route path="/collections/:slug" element={<Collection />} />
+                        <Route path="/search/:query" element={<Search />} />
+                    </Routes>
+                </Layout>
+            </Router>
+        </Provider>
+    </ErrorBoundary>
 
 );
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,4 @@
 export { Landing } from './landing/landing';
 export { Collection } from './collection/collection';
 export { Search } from './search/search';
+export { ErrorBoundary } from './error/errorBoundary';
