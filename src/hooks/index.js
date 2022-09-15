9c5d4698025e191a778032561e6bb5c9878f9099
Sina Shadbad <shadbad@gmail.com>
2022-09-15 13:44:38 +0100

    Add useJumpToTop hook

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -1,3 +1,4 @@
 export { useWindowResizeEffect } from 'hooks/useWindowResizeEffect/useWindowResizeEffect';
 export { useBodyLocker } from 'hooks/useBodyLocker/useBodyLocker';
 export { useOutsideClickDetector } from 'hooks/useOutsideClickDetector/useOutsideClickDetector';
+export { useJumpToTop } from 'hooks/useJumpToTop/useJumpToTop';
