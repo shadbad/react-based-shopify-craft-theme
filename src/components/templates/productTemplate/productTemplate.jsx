ca06fca11b8c651f4b2bbcc8cd0f4699a3ea0d65
Sina Shadbad <shadbad@gmail.com>
2022-09-19 13:46:42 +0100

    Update the buttonQuantity's event handler logic.

--- src/components/templates/productTemplate/productTemplate.jsx
+++ src/components/templates/productTemplate/productTemplate.jsx
@@ -26,6 +26,18 @@ const ProductTemplate = function ({ product, relatedProducts, banner }) {
 
             dispatch(cartActions.add(payload));
 
+        },
+
+        quantityAddition: () => {
+
+            if (quantity < product.stock) setQuantity(() => quantity + 1);
+
+        },
+
+        quantitySubtraction: () => {
+
+            if (quantity > 1) setQuantity(() => quantity - 1);
+
         }
 
     };
@@ -96,8 +108,9 @@ const ProductTemplate = function ({ product, relatedProducts, banner }) {
 
                             <ButtonQuantity
                                 className="product-template__info__cart__qty"
-                                quantity={quantity}
-                                setQuantity={setQuantity}
+                                value={quantity}
+                                handleAddition={handle.quantityAddition}
+                                handleSubtraction={handle.quantitySubtraction}
                                 min={1}
                                 max={product.stock}
                                 disabled={product.stock === 0}
