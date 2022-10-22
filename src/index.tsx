0d580067c5f6ae87f5e0e99703f68d8c561ea41b
Sina Shadbad <shadbad@gmail.com>
2022-10-22 10:34:34 +0100

    Update static test analysis config.

--- src/index.tsx
+++ src/index.tsx
@@ -4,13 +4,13 @@ import reportWebVitals from './reportWebVitals';
 import App from './app';
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+    document.getElementById('root') as HTMLElement
 );
 
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
