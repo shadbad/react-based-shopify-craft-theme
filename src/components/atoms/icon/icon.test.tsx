57284fc9195c8465f0f120c248d63fdc27937723
Sina Shadbad <shadbad@gmail.com>
2022-10-28 05:59:30 +0100

    Update the Icon atom

--- src/components/atoms/icon/icon.test.tsx
+++ src/components/atoms/icon/icon.test.tsx
@@ -3,7 +3,7 @@ import { Icon } from './icon';
 
 describe('Icon', () => {
     it('Renders an SVG', () => {
-        render(<Icon name="arrow-left" />);
-        expect(screen.getByTestId('icon-arrow-left')).toBeInTheDocument();
+        render(<Icon name="logo" />);
+        expect(screen.getByTestId('icon-logo')).toBeInTheDocument();
     });
 });
