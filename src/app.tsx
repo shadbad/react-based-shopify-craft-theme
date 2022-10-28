57702bc15e79741df207f92710e7f19176ba07b1
Sina Shadbad <shadbad@gmail.com>
2022-10-28 06:00:03 +0100

    Cleanup

--- src/app.tsx
+++ src/app.tsx
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { ErrorBoundary, Landing } from 'components/pages';
+import 'assets/styles/globals.scss';
 
 export default function App() {
     return (
